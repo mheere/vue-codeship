4e75e7beb4876ec71d125dfff40bcd80468124be
marcel heeremans <marcelheeremans@gmail.com>
2020-07-06 13:45:24 +0100

    update of test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,7 +3,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'Showing the power of Travis CI/CD!';
+    const msg = 'Showing the power of';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     });
